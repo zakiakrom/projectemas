02c4f9bca8083617083a0162a1c4ddde0c5ec63f
unknown <zakiakromnazih@gmail.com>
2025-12-24 12:08:05 +0300

    dri folder vscode

---
+++ coba.py
@@ -0,0 +1,26 @@
+# hafta 4
+
+a = 10
+b = 20
+ 
+print ("a sayisinin degregi: ")
+
+if a<b:
+    print ("a sayisi b sayisindan kucuktur ")
+
+elif a==b:
+    print ("a sayisi b sayisindan esittir ")
+
+else: 
+    print ("a sayisi b sayisindan esittir ")
+
+
+a = float (input("a sayisinin degregi: "))
+b = float (input("a sayisinin degregi: "))
+c = float (input("a sayisinin degregi: "))
+
+if a + b > c and  b + c > a and a + c > b :
+    print ("bener")
+else : 
+    print("salah")
+1
---
+++ zaaa.py
@@ -0,0 +1,11 @@
+a = b = c = 2
+print ('1. sayı=', a+c)
+print ('2. sayı=', b*c+a)
+print ('3. sayı=', c*7+2)
+
+sayi1=5
+sayi2='3'
+print (sayi1*sayi2)
+
+sayi1=("jancuk")
+print (sayi1, "ini adalah tipe data: " ,type(sayi1)) 
